24e766608fce8e9629b3251661368f706f37ca7c
aijazahmedbz <aijaz.ahmed.bz@gmail.com>
2023-06-21 08:32:22 +0500

    updated on 21-Jun-23

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -3,12 +3,17 @@ import { Todo } from "@/lib/drizzle";
 
 const getData = async () => {
   try {
-    const res = await fetch("http://127.0.0.1:3000/api/todo");
+    const res = await fetch("http://127.0.0.1:3000/api/todo", {
+      method: "GET",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     if (!res.ok) {
       throw new Error("Faild to fetch data");
     }
     const result = await res.json();
-
     return result;
   } catch (err) {
     console.log(err);
@@ -18,19 +23,18 @@ const getData = async () => {
 const TodoList = async () => {
   const res: { data: Todo[] } = await getData();
   return (
-    <>
+    <div className="max-h-[376px] overflow-auto">
       {res.data.map((item) => {
         return (
-          <div className="bg-gray-600 py-4 px-4 flex items-center gap-x-3 shadow rounded-lg my-3">
+          <div className="bg-white py-2 px-2 flex items-center gap-x-3 shadow rounded-full my-4">
             {/* Circle */}
-            <div className="h-3 w-3 bg-secondary rounded-full">
-              {/* Task Title */}
-              <p className="text-lg font-medium">{item.task}</p>
-            </div>
+            <div className="h-3 w-3 bg-secondary rounded-full"></div>
+            {/* Task Title */}
+            <p className="text-lg font-medium">{item.task}</p>
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import AddTodo from "./components/AddTodo";
 export default function Home() {
   return (
     <main className="bg-gradient-to-tr from-primary to-secondary h-screen flex justify-center items-center">
-      <div className="px-3 py-4 rounded-xl bg-white w-full max-w-md">
+      <div className="px-3 py-4 rounded-xl bg-gradiant-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60 backdrop-blur-xl w-full max-w-md">
         {/* @ts-ignor */}
         <TodoList />
         <AddTodo />
